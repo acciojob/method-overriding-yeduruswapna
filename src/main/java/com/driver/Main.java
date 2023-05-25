a294641532370a899128208f6ff34f048d232fb5
yeduruswapna <swapnayeduru@gmail.com>
2023-05-25 16:24:28 +0530

    Third Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,10 +15,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        A a= new A();
-        System.out.println(a.meth());
         B b = new B();
-        System.out.println(b.meth());
+        b.meth();
+        b.meth();
     }
   
 }
