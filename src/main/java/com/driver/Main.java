ad71eaed60e9061cc24485ded1af1b111abee569
yeduruswapna <swapnayeduru@gmail.com>
2023-05-25 16:06:36 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Override
+        String meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        A a= new A();
+        a.meth();
+        B b = new B();
+        b.meth();
+    }
   
 }
